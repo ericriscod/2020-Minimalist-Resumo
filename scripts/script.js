5fa94b4ec4b25432f350e5e453e47b7f1a43a757
Ericris Rossato <rossato.ep@gmail.com>
2023-05-04 23:11:22 -0300

    Inserted class theme-on

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,28 @@
-const myButton = document.querySelector('button')
-const icon = document.getElementById('icon')
+const myButton = document.querySelector('button');
 
-let modo = 'black'
+let modo = 'black';
 
+myButton.addEventListener('click', () => {
+  const theme = document.getElementsByClassName('theme');
 
-myButton.addEventListener('click', () =>{
-    
-}) 
+  if (modo === 'black') {
+    document.body.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+
+    for (let i = 0; i < theme.length; i++) {
+      theme[i].classList.add('theme-on');
+      console.log('Inserted theme');
+    }
+    modo = 'white';
+  } else {
+    document.body.style.backgroundColor = 'rgba(30, 30, 30, 1)';
+
+    const theme_on = document.getElementsByClassName('theme-on');
+    const size_theme_on = theme_on.length;
+
+    for (let i = 0; i < size_theme_on; i++) {
+      theme[i].classList.remove('theme-on');
+      console.log('removed theme');
+    }
+    modo = 'black';
+  }
+});
